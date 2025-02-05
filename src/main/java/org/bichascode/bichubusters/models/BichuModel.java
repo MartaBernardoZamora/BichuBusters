dd5c6c14085f6b317b716094d1550d2c98d02746
PFBregon <bregon@gmail.com>
2025-02-05 15:30:39 +0100

    test getName method created and failed

--- src/main/java/org/bichascode/bichubusters/models/BichuModel.java
+++ src/main/java/org/bichascode/bichubusters/models/BichuModel.java
@@ -2,4 +2,9 @@ package org.bichascode.bichubusters.models;
 
 public class BichuModel {
 
+    public String getName() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getName'");
+    }
+
 }
---
+++ src/test/java/org/bichascode/bichubusters/models/BichuModelTest.java
@@ -0,0 +1,22 @@
+package org.bichascode.bichubusters.models;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.instanceOf;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.is;
+
+public class BichuModelTest {
+
+    @Test
+    @DisplayName ("Testeo del método GetName")
+
+    void testGetName() {
+        String nameExpect = "fantasmico";
+        BichuModel bichuModel1 = new BichuModel();
+        String result = bichuModel1.getName();
+        assertThat(result, is(nameExpect));
+    }
+
+}
