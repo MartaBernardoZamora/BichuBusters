b31aab4c3361f114bc68fe9f2a1c429414dcab27
PFBregon <bregon@gmail.com>
2025-02-06 11:50:37 +0100

    Test GetAbility status ok

--- src/main/java/org/bichascode/bichubusters/models/BichuModel.java
+++ src/main/java/org/bichascode/bichubusters/models/BichuModel.java
@@ -41,8 +41,7 @@ public class BichuModel {
     }
 
     public String getAbility() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAbility'");
+        return ability;
     }
 
 }
