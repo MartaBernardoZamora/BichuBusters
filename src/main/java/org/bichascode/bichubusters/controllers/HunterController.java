088b8202ba414d52aa1041d3cba446fe52112ad6
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-07 11:53:22 +0100

    ok status test createHunter method

--- src/main/java/org/bichascode/bichubusters/controllers/HunterController.java
+++ src/main/java/org/bichascode/bichubusters/controllers/HunterController.java
@@ -1,12 +1,15 @@
 package org.bichascode.bichubusters.controllers;
 
+import java.util.ArrayList;
+
+import org.bichascode.bichubusters.models.BichuModel;
 import org.bichascode.bichubusters.models.HunterModel;
 
 public class HunterController {
 
     public HunterModel createHunter() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'createHunter'");
+        ArrayList<BichuModel> bichus = new ArrayList<>();
+        return new HunterModel(bichus);
     }
 
 }
