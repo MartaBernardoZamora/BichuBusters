5a2c1de46e374e7cccdf584c12d59336a42ebb4b
PFBregon <bregon@gmail.com>
2025-02-07 17:56:19 +0100

    Menu message in menuView created

--- src/main/java/org/bichascode/bichubusters/controllers/HunterController.java
+++ src/main/java/org/bichascode/bichubusters/controllers/HunterController.java
@@ -1,21 +1,25 @@
 package org.bichascode.bichubusters.controllers;
 
+import java.util.Scanner;
+
 import org.bichascode.bichubusters.models.BichuModel;
 import org.bichascode.bichubusters.models.HunterModel;
 
 public class HunterController {
 
     private HunterModel hunterModel;
-    
+        private Scanner scanner;
+
 
     public HunterController() {
         this.hunterModel = new HunterModel();
+        this.scanner = new Scanner(System.in);
     }
 
     public HunterModel getHunter() {
         return hunterModel;
     }
-    
+
     public void catchBichu(String name, int level, String type, String ability) {
         BichuModel bichuModel = new BichuModel(name, level, type, ability);
         hunterModel.getBichuList().add(bichuModel);
