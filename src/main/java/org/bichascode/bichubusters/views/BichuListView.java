b3fc8860da0d48ca4463494632469e5db15918cc
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-09 16:48:07 +0100

    BichuListView refactory to independent view

--- src/main/java/org/bichascode/bichubusters/views/BichuListView.java
+++ src/main/java/org/bichascode/bichubusters/views/BichuListView.java
@@ -30,8 +30,7 @@ public class BichuListView {
                     bichu.getAbility()
                 );            
             }
-        }       
-        hunterController.printMenuView();
+        }
     }
 
 }
