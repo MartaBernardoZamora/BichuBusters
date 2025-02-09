8cc3f30c1239fffec75885e215131f17d506d7bf
MartaBernardoZamora <martabernardozamora@gmail.com>
2025-02-09 17:02:12 +0100

    BichuListView refactory to format the table

--- src/main/java/org/bichascode/bichubusters/views/BichuListView.java
+++ src/main/java/org/bichascode/bichubusters/views/BichuListView.java
@@ -30,6 +30,7 @@ public class BichuListView {
                     bichu.getAbility()
                 );            
             }
+            System.out.println("==================================================================\n");
         }
     }
 
